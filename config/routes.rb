0ea54e4db29e5221f02501045f369577b9855aae
khalil <>
2018-11-27 17:37:13 +0100

    khalil

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   post 'home/index'
   post 'home/show'
   root 'home#index'
+  get 'home/profil'
+  post 'home/profil'
   resources :show
+  resources :profil
   devise_for :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,15 +2,14 @@ require 'faker'
 require 'nokogiri'
 require 'open-uri'
 
-    doc = Nokogiri::HTML(open("https://burst.shopify.com/cat"))
+    doc = Nokogiri::HTML(open("https://wall.alphacoders.com/search.php?lang=French&search=chat"))
     @taburl = []
-    @tabtitle = []
+    @tabtitle = ["chat dorée","chat chat","un petit chat trés mignion","chat gris","chat de la vega","chat petit","chat saute","chat de se chat"]
     myhash = {}
     20.times do
-        doc.css('.photo-tile__image-wrapper @href').each do |url| @tabtitle << url.text.delete('/')[6..-1].gsub!('-',' ') end
-        doc.css('.photo-tile__image-wrapper @href').each do |url| @taburl << "https://burst.shopify.com#{url.text}" end
+        doc.css('.boxgrid img @src').each do |url| @taburl << url.text end
     end
 
-    20.times do |y|
-        @items = Item.create(title:@tabtitle[y], description:Faker::Lorem.paragraph, price:rand(1.00..10), image_url:@taburl[y])
+    8.times do |y|
+        @items = Item.create(title:@tabtitle[y], description:Faker::Lorem.paragraph, price:rand(1.00...10), image_url:@taburl[y])
     end
