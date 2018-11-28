a092d09c08bbd10be1fba4ca7bf4bb20aa1032e7
Salim Boumediene <salimboumediene@gmail.com>
2018-11-28 17:17:00 +0100

    modif home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,10 @@
 class HomeController < ApplicationController
-<<<<<<< HEAD
   protect_from_forgery with: :exception
   helper_method :current_order
-=======
-  include CurrentCart
-  before_action :set_cart, only: [:create]
->>>>>>> master
   def index
     @items = Item.all
   end
 
-<<<<<<< HEAD
 
   def current_order
     if !session[:item_id].nil?
@@ -18,10 +12,6 @@ class HomeController < ApplicationController
     else
       Item.new
     end
-=======
-  def create
-    @item = Item.find(params[:id])
->>>>>>> master
   end
 
 end
